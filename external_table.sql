87744021472a8824eb5893f3c892d912e54c42e9
dprzek <79841286+dprzek@users.noreply.github.com>
2025-03-03 14:16:52 +0100

    Update external_table.sql

--- external_table.sql
+++ external_table.sql
@@ -2,6 +2,6 @@ CREATE EXTERNAL TABLE `your_set.objects`
 WITH CONNECTION `us.llm-connection`
 OPTIONS(
   object_metadata = 'SIMPLE',
-  uris = ['gs://bqml_vision/*'],
+  uris = ['gs://bqml_vision/*'], #it is URI to Cloud Storage and contains bunch of .jpg files
   max_staleness = INTERVAL 30 MINUTE,
   metadata_cache_mode = 'AUTOMATIC');
