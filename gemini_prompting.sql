44566cdde1fc077ab729cc05275648af2d744436
dprzek <79841286+dprzek@users.noreply.github.com>
2025-03-03 14:19:45 +0100

    Update gemini_prompting.sql

--- gemini_prompting.sql
+++ gemini_prompting.sql
@@ -3,8 +3,8 @@ SELECT
    ml_generate_text_llm_result as description
  FROM
    ML.GENERATE_TEXT(
-     MODEL `gemini_vision.gemini_pro_vision_model`,
-     TABLE `gemini_vision.jpg_objects3`,
+     MODEL `your_set.modelgraphics`,
+     TABLE `your_set.objects`,
       STRUCT(
        'What is in the picture?' AS PROMPT,
        TRUE AS FLATTEN_JSON_OUTPUT));
