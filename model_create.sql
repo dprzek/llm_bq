37753967b7aefe0bcf45cb36a7000326601f7c04
dprzek <79841286+dprzek@users.noreply.github.com>
2025-03-03 14:20:08 +0100

    Update model_create.sql

--- model_create.sql
+++ model_create.sql
@@ -1,3 +1,3 @@
-CREATE OR REPLACE MODEL your_set.llm_model
+CREATE OR REPLACE MODEL your_set.modelgraphics
   REMOTE WITH CONNECTION `us.llm-connection`
   OPTIONS (ENDPOINT = 'gemini-1.5-flash-001');
